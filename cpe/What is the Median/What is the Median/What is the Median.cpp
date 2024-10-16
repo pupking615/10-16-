249b682d4f752bc5578a176ae9edfb3c146ad1a4
pupking615 <41243120@nfu.edu.tw>
2024-10-16 21:51:17 +0800

    Add files via upload

---
+++ cpe/What is the Median/What is the Median/What is the Median.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, cnt = 0;
+    int m1, m2,t;
+    int num[10000];  
+    while (cin >> n) {
+        num[cnt++] = n;
+        for (int i = 0; i < cnt; i++) {
+            for (int j = i + 1; j < cnt; j++) {
+                if (num[i] > num[j]) {
+                    t = num[i];
+                    num[i] = num[j];
+                    num[j] = t;
+                }
+            }
+        }
+        if (cnt % 2 == 0) {
+            m1 = num[cnt / 2 - 1];
+            m2 = num[cnt / 2];
+            cout << (m1 + m2) / 2 << endl;
+        }
+        else {
+            cout << num[cnt / 2] << endl;
+        }
+    }
+    return 0;
+}
+
+
---
+++ cpe/What's Cryptanalysis/What's Cryptanalysis/What's Cryptanalysis.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t; 
+    string st;
+    int count[26] = { 0 };  
+     for (int i = 0; i < t; i++)
+     {
+        cin >> ws;  
+        getline(cin, st);
+        for (int j = 0; j < st.length(); j++) {
+            char c = st[j];
+            if (isalpha(c)) {
+                c = toupper(c);  
+                count[c - 'A']++; 
+            }
+        }
+    }
+    for (int i = 0; i < 26; i++) {
+        if (count[i] > 0) {
+            cout << char('A' + i) << " " << count[i] << endl;
+        }
+    }
+    return 0;
+}
+
